d5054c45fb66cef5996c009059b447945f2b5715
Obimbo07 <obimboausts@gmail.com>
2024-02-07 16:51:41 +0300

    fixed order of migration

--- db/migrate/20240207114731_create_foods.rb
+++ db/migrate/20240207114731_create_foods.rb
@@ -3,7 +3,6 @@ class CreateFoods < ActiveRecord::Migration[7.0]
     create_table :foods do |t|
       t.string :name
       t.string :measurement_unit
-      t.integer :quantity
       t.decimal :price
 
       t.timestamps
