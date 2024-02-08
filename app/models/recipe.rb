bc922502c1a7c37e187841b23215ead5a05ae765
Obimbo07 <obimboausts@gmail.com>
2024-02-08 18:42:13 +0300

    generated recipe resource

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,11 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
-  has_many :foods
+  has_many :foods, through: :recipe_foods, class_name: 'Food'
 
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :preparation_time, presence: true
-  validates :cooking_time, presence: true
-  validates :public, inclusion: { in: [true, false] }
+  scope :public_recipes, -> { where(public: true) }
+
+  def total_price
+    foods.sum(:price)
+  end
 end
