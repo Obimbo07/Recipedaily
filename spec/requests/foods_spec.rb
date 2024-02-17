c01d36eda235ebdad55ceba19f16d92efbe6897e
Alier <alierditmaguet@gmail.com>
2024-02-17 05:20:38 +0200

    Add Request tests

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,110 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe '/recipes', type: :request do
-  let(:user) { User.create(email: 'abc@example.com', password: 'password') }
-
+RSpec.describe '/foods', type: :request do
   let(:valid_attributes) do
-    {
-      name: 'test',
-      description: 'Testing',
-      cooking_time: 1,
-      preparation_time: 2,
-      user:
-    }
+    { name: 'Test Food', measurement_unit: 'kg', quantity: 10, price: 5.0 }
   end
 
-  let(:invalid_attributes) do
-    {
-      name: 'test',
-      description: 'Testing',
-      cooking_time: 1,
-      preparation_time: 2
-    }
+  let(:valid_user_attributes) do
+    { name: 'Test User', email: 'user@example.com', password: 'password', password_confirmation: 'password' }
   end
 
-  before(:each) do
-    login_as(user, scope: :user)
+  let(:user) do
+    user = User.create! valid_user_attributes
+    # Confirm the user's email if necessary
+    user.confirm if user.respond_to?(:confirm)
+    sign_in user
+    user
   end
 
   describe 'GET /index' do
     it 'renders a successful response' do
-      Recipe.create! valid_attributes
-      get recipes_url
+      Food.create! valid_attributes.merge(user:)
+      get foods_url
       expect(response).to be_successful
-    end
-  end
-  describe 'GET /' do
-    it 'should get public recipes' do
-      get '/'
-      assert_response :success
-    end
-  end
-
-  describe 'GET /show' do
-    it 'renders a successful response' do
-      recipe = Recipe.create! valid_attributes
-      get recipe_url(recipe)
-      expect(response).to be_successful
-    end
-  end
-
-  describe 'GET /show' do
-    it 'should update recipe as public' do
-      recipe = Recipe.create! valid_attributes
-      put toggle_public_recipe_url(recipe.id), params: { recipe: { public: true } }, as: :json
-      assert_response :success
-      recipe.reload
-      assert recipe.public
-    end
-  end
-
-  describe 'GET /new' do
-    it 'renders a successful response' do
-      get new_recipe_url
-      expect(response).to be_successful
-    end
-  end
-
-  describe 'POST /create' do
-    context 'with valid parameters' do
-      it 'creates a new Recipe' do
-        expect do
-          post recipes_url, params: { recipe: valid_attributes }
-        end
-      end
-
-      it 'redirects to the created recipe' do
-        post recipes_url, params: { recipe: valid_attributes }
-        expect(response).to redirect_to(recipe_url(Recipe.last))
-      end
-    end
-
-    context 'with invalid parameters' do
-      it 'does not create a new Recipe' do
-        expect do
-          post recipes_url, params: { recipe: invalid_attributes }
-        end
-      end
-
-      it "renders a response with 422 status (i.e. to display the 'new' template)" do
-        post recipes_url, params: { recipe: invalid_attributes }
-        expect(response).to have_http_status(302)
-      end
     end
   end
 
   describe 'DELETE /destroy' do
-    it 'destroys the requested recipe' do
-      recipe = Recipe.create! valid_attributes
+    it 'destroys the requested food' do
+      food = Food.create! valid_attributes.merge(user:)
       expect do
-        delete recipe_url(recipe)
-      end
-    end
-
-    it 'redirects to the recipes list' do
-      recipe = Recipe.create! valid_attributes
-      delete recipe_url(recipe)
-      expect(response).to redirect_to(recipes_url)
+        delete food_url(food)
+      end.to change(Food, :count).by(-1)
     end
   end
 end
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,51 +1,97 @@
 require 'rails_helper'
 
-RSpec.describe 'Recipes', type: :request do
-  let(:user) { User.create!(email: 'testing@recipedaily.com', password: 'password123') }
+RSpec.describe '/recipes', type: :request do
+  let(:user) { User.create(email: 'abc@example.com', password: 'password', confirmed_at: Time.now) } # Confirm the user
 
   let(:valid_attributes) do
     {
-      name: 'Chicken Alfredo',
-      description: 'A creamy pasta dish with chicken and parmesan cheese',
+      name: 'test',
+      description: 'Testing',
       cooking_time: 1,
       preparation_time: 2,
-      user: # Associate the user with the recipe
+      user_id: user.id # Assign the user id
     }
+  end
+
+  let(:invalid_attributes) do
+    {
+      name: nil,
+      description: 'Testing',
+      cooking_time: 1,
+      preparation_time: 2,
+      user_id: user.id # Assign the user id
+    }
+  end
+
+  before(:each) do
+    login_as(user, scope: :user)
   end
 
   describe 'GET /index' do
     it 'renders a successful response' do
-      Recipe.create!(valid_attributes)
+      Recipe.create! valid_attributes
       get recipes_url
-      follow_redirect! # Follow any redirects
-
       expect(response).to be_successful
     end
   end
 
-  describe 'GET/ ' do
-    it 'should render public recipes' do
+  describe 'GET /' do
+    it 'should get public recipes' do
       get '/'
-      assert_response :success
+      expect(response).to be_successful # Use RSpec's expect method
     end
   end
 
   describe 'GET /show' do
     it 'renders a successful response' do
-      recipe = Recipe.create!(valid_attributes)
+      recipe = Recipe.create! valid_attributes
       get recipe_url(recipe)
-      follow_redirect! # Follow any redirects
-
       expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /show' do
+    it 'should update recipe as public' do
+      recipe = Recipe.create! valid_attributes
+      put toggle_public_recipe_url(recipe.id), params: { recipe: { public: true } }, as: :json
+      expect(response).to be_successful # Use RSpec's expect method
+      recipe.reload
+      expect(recipe.public).to be_truthy # Use RSpec's expect method
     end
   end
 
   describe 'GET /new' do
     it 'renders a successful response' do
       get new_recipe_url
-      follow_redirect! # Follow any redirects
-
       expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Recipe' do
+        expect do
+          post recipes_url, params: { recipe: valid_attributes }
+        end.to change(Recipe, :count).by(1) # Use RSpec's change matcher
+      end
+
+      it 'redirects to the created recipe' do
+        post recipes_url, params: { recipe: valid_attributes }
+        expect(response).to redirect_to(recipe_url(Recipe.last))
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new Recipe' do
+        expect do
+          post recipes_url, params: { recipe: invalid_attributes }
+        end.to change(Recipe, :count).by(0) # Use RSpec's change matcher
+      end
+
+      it "renders a response with 4222 status (i.e. to display the 'new' template)" do
+        post recipes_url, params: { recipe: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
     end
   end
 
@@ -54,7 +100,13 @@ RSpec.describe 'Recipes', type: :request do
       recipe = Recipe.create! valid_attributes
       expect do
         delete recipe_url(recipe)
-      end
+      end.to change(Recipe, :count).by(-1) # Use RSpec's change matcher
+    end
+
+    it 'redirects to the recipes list' do
+      recipe = Recipe.create! valid_attributes
+      delete recipe_url(recipe)
+      expect(response).to redirect_to(recipes_url)
     end
   end
 end
